561b67ffa469847da97c0ee90c74fcc810f4ae46
kahmnkk <kyuhh1214@naver.com>
2022-03-07 23:10:02 +0900

    add. programmers practice
    - 타겟 넘버

---
+++ programmers-practice/타겟넘버.js
@@ -0,0 +1,10 @@
+// 프로그래머스 연습문제 - 타겟 넘버
+// https://programmers.co.kr/learn/courses/30/lessons/43165
+// DFS 사용
+
+function solution(n, times) {
+    var answer = 0;
+    return answer;
+}
+
+console.log(solution(6, [7, 10], 28));
