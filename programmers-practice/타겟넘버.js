305123e0d530e9369e9d348ab00e404147fe84bb
kahmnkk <kyuhh1214@naver.com>
2022-03-07 23:10:47 +0900

    add. programmers practice
    - 타겟 넘버

--- programmers-practice/타겟넘버.js
+++ programmers-practice/타겟넘버.js
@@ -2,9 +2,33 @@
 // https://programmers.co.kr/learn/courses/30/lessons/43165
 // DFS 사용
 
-function solution(n, times) {
-    var answer = 0;
+let answer = 0;
+let numbersArr = 0;
+let targetNum = 0;
+
+function solution(numbers, target) {
+    numbersArr = numbers;
+    targetNum = target;
+
+    recursion(0, []);
+
     return answer;
 }
 
-console.log(solution(6, [7, 10], 28));
+function recursion(idx, result) {
+    if (idx == numbersArr.length) {
+        let num = 0;
+        for (let i in result) {
+            if (result[i] == true) num += numbersArr[i];
+            else num -= numbersArr[i];
+        }
+        if (num == targetNum) {
+            answer++;
+        }
+    } else {
+        recursion(idx + 1, result.slice().concat([true]));
+        recursion(idx + 1, result.slice().concat([false]));
+    }
+}
+
+console.log(solution([1, 1, 1, 1, 1], 3));
